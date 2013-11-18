b811015da01700632d97c3c872086c2d1b33e87b
Stefan Sidler <ste.sidler@gmail.com>
2013-11-18 18:08:49 +0100

    ver 10

--- Veranstaltung10/index.php
+++ Veranstaltung10/index.php
@@ -1,32 +1,12 @@
-<?php
+<html>
+<head>
 
-$url ="http://webdev/Selbststudium/Veranstaltung10/resolver.php?service=kantone&methode=list";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-$body = curl_exec($ch);
-curl_close($ch);
-// Via json
-$json = json_decode($body);
-
-
-echo "<br>";
-echo "<br>";
-
-$url ="http://webdev/Selbststudium/Veranstaltung10/resolver.php?service=kantone&methode=single&id=ZH";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-$body = curl_exec($ch);
-curl_close($ch);
-// Via json
-$json = json_decode($body);
-
-echo "<br>";
-echo "<br>";
-
-$url ="http://webdev/Selbststudium/Veranstaltung10/resolver.php?service=kantone&methode=list&sort=Kanton";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-$body = curl_exec($ch);
-curl_close($ch);
-// Via json
-$json = json_decode($body);
+</head>
+<body>
+	<h1>Samples</h1>
+	<ul>
+		<li>http://[SERVER_PATH]/resolver.php?service=kantone&methode=list&sort=Kanton</li>
+		<li>http://[SERVER_PATH]/resolver.php?service=kantone&methode=single&id=SZ</li>
+	</ul>
+</body>
+</html>
