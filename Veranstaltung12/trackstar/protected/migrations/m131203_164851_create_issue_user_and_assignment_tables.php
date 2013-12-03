02594c248c959804ca86e6c2b22361082a45fc77
Stefan Sidler <ste.sidler@gmail.com>
2013-12-03 18:08:45 +0100

    crud

--- Veranstaltung12/trackstar/protected/migrations/m131203_164851_create_issue_user_and_assignment_tables.php
+++ Veranstaltung12/trackstar/protected/migrations/m131203_164851_create_issue_user_and_assignment_tables.php
@@ -2,16 +2,6 @@
 
 class m131203_164851_create_issue_user_and_assignment_tables extends CDbMigration
 {
-	public function up()
-	{
-	}
-
-	public function down()
-	{
-		echo "m131203_164851_create_issue_user_and_assignment_tables does not support migration down.\n";
-		return false;
-	}
-
 	// Use safeUp/safeDown to do migration with transaction
      public function safeUp()
      {
